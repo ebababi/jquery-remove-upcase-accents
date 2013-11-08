ebababi/jquery-remove-upcase-accents

commit be19ccbf0301cedefce53561dea6606a63ed3654

Extended to support input elements

jquery.remove-upcase-accents.js
@@ -37,7 +37,7 @@ jQuery.extend(jQuery.expr[":"], {
 
 jQuery.extend({
 	removeAcc: function( elem ) {
-		var text = elem.innerHTML;
+		var text = (elem.tagName.toLowerCase() == "input") ? elem.value : elem.innerHTML;
 
 		text = text.replace( /Ά/g, "Α" );
 		text = text.replace( /ά/g, "α" );
@@ -60,7 +60,7 @@ jQuery.extend({
 		text = text.replace( /Ώ/g, "Ω" );
 		text = text.replace( /ώ/g, "ω" );
 
-		elem.innerHTML = text;
+		(elem.tagName.toLowerCase() == "input") ? (elem.value = text) : (elem.innerHTML = text);
 	}
 });
 

commit e06d3bcbfe166b425ed27625d3ae2862baba4425

Undefined function issue introduced by be19ccb resolved. Fixes #2

jquery.remove-upcase-accents.js
@@ -37,7 +37,7 @@ jQuery.extend(jQuery.expr[":"], {
 
 jQuery.extend({
 	removeAcc: function( elem ) {
-		var text = elem.is( "input" ) ? elem.value : elem.innerHTML;
+		var text = jQuery( elem ).is( "input" ) ? elem.value : elem.innerHTML;
 
 		text = text.replace( /\u0386/g, "\u0391" ); // 'Ά':'Α'
 		text = text.replace( /\u0388/g, "\u0395" ); // 'Έ':'Ε'
@@ -60,7 +60,7 @@ jQuery.extend({
 		text = text.replace( /\u03CD/g, "\u03C5" ); // 'ύ':'υ'
 		text = text.replace( /\u03CE/g, "\u03C9" ); // 'ώ':'ω'
 
-		elem.is( "input" ) ? ( elem.value = text ) : ( elem.innerHTML = text );
+		jQuery( elem ).is( "input" ) ? ( elem.value = text ) : ( elem.innerHTML = text );
 	}
 });
 