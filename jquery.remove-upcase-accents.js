99f57b864947436d783b755e12300d28244f8261
Nikolaos Anastopoulos <ebababi@ebababi.net>
2013-11-08 11:39:20 +0200

    Redesigned to transform only element values and first level text nodes

--- jquery.remove-upcase-accents.js
+++ jquery.remove-upcase-accents.js
@@ -37,30 +37,42 @@ jQuery.extend(jQuery.expr[":"], {
 
 jQuery.extend({
 	removeAcc: function( elem ) {
-		var text = jQuery( elem ).is( "input" ) ? elem.value : elem.innerHTML;
+		// Replace uppercase accented greek characters
+		function removeAccEL( text ) {
+			return typeof text !== "string" ?
+				// handle cases that text is not a string
+				text :
+				// global replace of uppercase accented characters
+				text.
+					replace( /\u0386/g, "\u0391" ). // 'Ά':'Α'
+					replace( /\u0388/g, "\u0395" ). // 'Έ':'Ε'
+					replace( /\u0389/g, "\u0397" ). // 'Ή':'Η'
+					replace( /\u038A/g, "\u0399" ). // 'Ί':'Ι'
+					replace( /\u038C/g, "\u039F" ). // 'Ό':'Ο'
+					replace( /\u038E/g, "\u03A5" ). // 'Ύ':'Υ'
+					replace( /\u038F/g, "\u03A9" ). // 'Ώ':'Ω'
+					replace( /\u0390/g, "\u03B9" ). // 'ΐ':'ι'
+					replace( /\u03AA/g, "\u0399" ). // 'Ϊ':'Ι'
+					replace( /\u03AB/g, "\u03A5" ). // 'Ϋ':'Υ'
+					replace( /\u03AC/g, "\u03B1" ). // 'ά':'α'
+					replace( /\u03AD/g, "\u03B5" ). // 'έ':'ε'
+					replace( /\u03AE/g, "\u03B7" ). // 'ή':'η'
+					replace( /\u03AF/g, "\u03B9" ). // 'ί':'ι'
+					replace( /\u03B0/g, "\u03C5" ). // 'ΰ':'υ'
+					replace( /\u03CA/g, "\u03B9" ). // 'ϊ':'ι'
+					replace( /\u03CB/g, "\u03C5" ). // 'ϋ':'υ'
+					replace( /\u03CC/g, "\u03BF" ). // 'ό':'ο'
+					replace( /\u03CD/g, "\u03C5" ). // 'ύ':'υ'
+					replace( /\u03CE/g, "\u03C9" ); // 'ώ':'ω'
+		}
 
-		text = text.replace( /\u0386/g, "\u0391" ); // 'Ά':'Α'
-		text = text.replace( /\u0388/g, "\u0395" ); // 'Έ':'Ε'
-		text = text.replace( /\u0389/g, "\u0397" ); // 'Ή':'Η'
-		text = text.replace( /\u038A/g, "\u0399" ); // 'Ί':'Ι'
-		text = text.replace( /\u038C/g, "\u039F" ); // 'Ό':'Ο'
-		text = text.replace( /\u038E/g, "\u03A5" ); // 'Ύ':'Υ'
-		text = text.replace( /\u038F/g, "\u03A9" ); // 'Ώ':'Ω'
-		text = text.replace( /\u0390/g, "\u03B9" ); // 'ΐ':'ι'
-		text = text.replace( /\u03AA/g, "\u0399" ); // 'Ϊ':'Ι'
-		text = text.replace( /\u03AB/g, "\u03A5" ); // 'Ϋ':'Υ'
-		text = text.replace( /\u03AC/g, "\u03B1" ); // 'ά':'α'
-		text = text.replace( /\u03AD/g, "\u03B5" ); // 'έ':'ε'
-		text = text.replace( /\u03AE/g, "\u03B7" ); // 'ή':'η'
-		text = text.replace( /\u03AF/g, "\u03B9" ); // 'ί':'ι'
-		text = text.replace( /\u03B0/g, "\u03C5" ); // 'ΰ':'υ'
-		text = text.replace( /\u03CA/g, "\u03B9" ); // 'ϊ':'ι'
-		text = text.replace( /\u03CB/g, "\u03C5" ); // 'ϋ':'υ'
-		text = text.replace( /\u03CC/g, "\u03BF" ); // 'ό':'ο'
-		text = text.replace( /\u03CD/g, "\u03C5" ); // 'ύ':'υ'
-		text = text.replace( /\u03CE/g, "\u03C9" ); // 'ώ':'ω'
-
-		jQuery( elem ).is( "input" ) ? ( elem.value = text ) : ( elem.innerHTML = text );
+		jQuery( elem ).each(function() {
+			this.value = removeAccEL( this.value );
+		}).contents().filter(function() {
+			return this.nodeType === 3; // Node.TEXT_NODE
+		}).each(function() {
+			this.nodeValue = removeAccEL( this.nodeValue );
+		});
 	}
 });
 
